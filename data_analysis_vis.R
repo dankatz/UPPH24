2c7d875d2389f4cc65b7c1d56594928c06938b77
dankatz <dankatz512@gmail.com>
2024-11-25 10:34:32 -0500

    adding outline for analysis script

--- data_analysis_vis.R
+++ data_analysis_vis.R
@@ -1,1 +1,31 @@
 #R script for data visualization and analysis
+
+#set up work environment
+library(googledrive)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(here)
+
+here()
+
+### download output from i-Tree Eco analyses on Google Drive ###################
+#manually download the folder from google drive and place it in the folder
+
+### assemble dataframe for analysis ############################################
+
+
+
+### figure: effects of trees over time for each exposure #######################
+
+
+
+### figure: visualize changes in the number and abundance of each species ######
+
+
+
+### figure: human vulnerability over time ######################################
+
+
+
+### figure: predictions in changes of ES/D in near future ######################
